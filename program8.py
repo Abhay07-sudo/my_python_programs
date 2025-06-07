41ed3410312e223088de0993375de1d9cc6a989d
Abhay07-sudo <abhaysingha2050@gmail.com>
2025-06-07 19:48:46 +0530

    Create program8.py

---
+++ program8.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("a =", a)
+print("b =", b)
+
+# Logical AND
+print("a > 0 and b > 0:", a > 0 and b > 0)
+
+# Logical OR
+print("a > 0 or b > 0:", a > 0 or b > 0)
+
+# Logical NOT
+print("not(a > 0):", not(a > 0))
