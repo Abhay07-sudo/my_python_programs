d938fd875e58e3dcb61c1d331f0f315a9f2bc7fc
Abhay07-sudo <abhaysingha2050@gmail.com>
2025-06-07 19:51:09 +0530

    Create program10.py

---
+++ program10.py
@@ -0,0 +1,7 @@
+my_list = ["1", "2", "3", "4"]
+
+# Convert each element from string to integer
+int_list = [int(x) for x in my_list]
+
+print("Original list (strings):", my_list)
+print("Converted list (integers):", int_list)
